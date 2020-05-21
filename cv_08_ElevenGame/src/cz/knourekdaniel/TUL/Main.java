62b07592be20bc825f22b97b9a518ccf5e3a9e23
DanielKnourek <dev.danielknourek@gmail.com>
2020-05-21 16:48:45 +0200

    Eleven Game init

---
+++ cv_08_ElevenGame/src/cz/knourekdaniel/TUL/Main.java
@@ -0,0 +1,8 @@
+package cz.knourekdaniel.TUL;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ cv_08_ElevenGame/src/cz/knourekdaniel/TUL/elevengame/GameRunner.java
@@ -0,0 +1,14 @@
+package cz.knourekdaniel.TUL.elevengame;
+
+/**
+ * Runs GUI or ConsolaUI
+ * @author janvit
+ */
+public class GameRunner {
+    
+    public static void main(String[] args) {
+        ConsolaUI game = new ConsolaUI();
+        //GUI game = new GUI();
+        game.start();
+    }
+}
